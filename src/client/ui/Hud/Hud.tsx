bd02d8561771716bada9ed34ced4669152fbe581
Bill Chen <user@sousvide.cx>
2025-10-02 23:19:10 -0700

    Crossshair

--- src/client/ui/Hud/Hud.tsx
+++ src/client/ui/Hud/Hud.tsx
@@ -1,10 +1,12 @@
 import React from "@rbxts/react";
 import { Health } from "./components/Health";
 import { Hotbar } from "./components/Hotbar";
+import { Crosshair } from "./components/Crosshair";
 
 export function Hud() {
     return (
         <>
+            <Crosshair />
             <Hotbar />
             <Health />
         </>
