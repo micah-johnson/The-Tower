8dbae4bc8c056cff9cea58d3244d7345b315dce2
micah-johnson <57875046+micah-johnson@users.noreply.github.com>
2025-10-07 01:18:03 -0700

    fuck crosshair

--- src/client/ui/Hud/Hud.tsx
+++ src/client/ui/Hud/Hud.tsx
@@ -1,13 +1,11 @@
 import React from "@rbxts/react";
 import { Health } from "./components/Health";
 import { Hotbar } from "./components/Hotbar";
-import { Crosshair } from "./components/Crosshair";
 import { MovementSpeed } from "./components/MovementSpeed";
 
 export function Hud() {
     return (
         <>
-            <Crosshair />
             <Hotbar />
             <Health />
             <MovementSpeed />
